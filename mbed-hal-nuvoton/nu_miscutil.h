d245667c05cc56a88e7b585ab14588a530013ef3
ccli8 <ccli8@nuvoton.com>
2015-11-25 16:11:42 +0800

    Add nu_nop() for delay function

--- mbed-hal-nuvoton/nu_miscutil.h
+++ mbed-hal-nuvoton/nu_miscutil.h
@@ -25,6 +25,49 @@ extern "C" {
 #define NU_MIN(a,b) ((a)<(b)?(a):(b))
 #define NU_CLAMP(x, min, max)   NU_MIN(NU_MAX((x), (min)), (max))
 
+__STATIC_INLINE void nu_nop(uint32_t n)
+{
+    uint32_t times = n / 10;
+    uint32_t rmn = n % 10;
+    
+    while (times --) {
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+        __NOP();
+    }
+    
+    switch (rmn) {
+        case 9:
+            __NOP();
+        case 8:
+            __NOP();
+        case 7:
+            __NOP();
+        case 6:
+            __NOP();
+        case 5:
+            __NOP();
+        case 4:
+            __NOP();
+        case 3:
+            __NOP();
+        case 2:
+            __NOP();
+        case 1:
+            __NOP();
+        default:
+            break;
+    }
+}
+    
+    
 #ifdef __cplusplus
 }
 #endif
